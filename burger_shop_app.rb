9ed77ea9d6ce62177ae8e770dfeffd5f6d1b8408
wvfeders <wvfedders@frontier.com>
2018-01-23 14:50:35 -0500

    working with many changes

--- burger_shop_app.rb
+++ burger_shop_app.rb
@@ -26,3 +26,28 @@ end
 
  	erb :results, :locals => {:hamburgers => hamburgers, :cheeseburgers => cheeseburgers, :hotdogs => hotdogs, :chilidogs => chilidogs}
  end	
+
+ post '/payment' do
+	tamount = params[:tamount]
+	tax = params[:tax]
+	total_with_tax = params[:total_with_tax]
+	payment_amount = params[:payment_amount]
+	hamburgers = params[:hamburgers]
+	cheeseburgers = params[:cheeseburgers]
+	hotdogs = params[:hotdogs]
+	chilidogs = params[:chilidogs]
+
+	redirect 'make_change?tamount=' + tamount + '&tax=' + tax + '&total_with_tax=' + total_with_tax + '&payment_amount=' + payment_amount + '&hamburgers=' + hamburgers + '&cheeseburgers=' + cheeseburgers + '&hotdogs=' + hotdogs + '&chilidogs=' + chilidogs
+end
+
+get '/make_change' do
+	tamount = params[:tamount]
+	tax = params[:tax]
+	total_with_tax = params[:total_with_tax]
+	payment_amount = params[:payment_amount]
+	hamburgers = params[:hamburgers]
+	cheeseburgers = params[:cheeseburgers]
+	hotdogs = params[:hotdogs]
+	chilidogs = params[:chilidogs]
+	erb :final, :locals => {:tamount => tamount, :tax => tax, :total_with_tax => total_with_tax, :payment_amount => payment_amount, :hamburgers => hamburgers, :cheeseburgers => cheeseburgers, :hotdogs => hotdogs, :chilidogs => chilidogs}
+end	
